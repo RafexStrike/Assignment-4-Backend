b0da98dfc8cb96d092b6e0b5fa86901afac9edf2
rafexstrike <adnanrahmanrafi515@gmail.com>
2026-01-29 05:27:40 +0600

    admin added via seeding

---
+++ src/script/seedAdmin.ts
@@ -0,0 +1,57 @@
+// src/script/seedAdmin.ts
+import { prisma } from "../lib/prisma";
+import { userRole } from "../middleware/auth.middleware";
+
+async function seedAdmin() {
+  try {
+    // check if the user already exist in the database or not
+    const adminData = {
+      name: "Skillbridge Admin",
+      email: "admin@skillbridge.com",
+      role: userRole.ADMIN,
+      //   role: "ADMIN",
+      password: "admin1234",
+    };
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: adminData.email,
+      },
+    });
+
+    if (existingUser) {
+      throw new Error("User already exists!");
+    }
+
+    const signUpAdmin = await fetch(
+      "http://localhost:5000/api/auth/sign-up/email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Origin: "http://localhost:3000",
+        },
+        body: JSON.stringify(adminData),
+      },
+    );
+
+    console.log(signUpAdmin);
+
+    // if (signUpAdmin.ok) {
+    //   console.log("*** Admin created ***");
+    //   await prisma.user.update({
+    //     where: {
+    //       email: adminData.email,
+    //     },
+    //     data: {
+    //       emailVerified: true,
+    //     },
+    //   });
+    //   console.log("*** Email verification status updated! ***");
+    // }
+    console.log("******* SUCCESS ******");
+  } catch (error: any) {
+    console.log("error message:", error.message);
+  }
+}
+
+seedAdmin();
